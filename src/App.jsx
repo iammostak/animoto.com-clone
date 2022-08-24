15990de0f76e64b379720bb16e48252ecb0f0357
iammostak <mdmostafijur85@gmail.com>
2022-08-24 10:01:17 +0530

    favicon path modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+	const [display, setDisplay] = useState(false);
+
 	return (
 		<div className="App">
-
+			
 		</div>
 	);
 }
